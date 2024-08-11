2a77939a55919f895ebd6eeda1176be6361250a3
jamitz440 <jamster44085@gmail.com>
2024-08-11 15:32:11 +0100

    updates

--- src/env.js
+++ src/env.js
@@ -20,7 +20,6 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_ISBN_AUTH: z.string(),
   },
 
   /**
@@ -30,7 +29,6 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    ISBN_AUTH: process.env.ISBN_AUTH,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
