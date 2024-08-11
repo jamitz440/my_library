24f0a4c3674102fd24028a24498d3eee00763c98
jamitz440 <jamster44085@gmail.com>
2024-08-11 15:10:02 +0100

    auth updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,97 @@
 import React, { useState } from "react";
 import { BarcodeScanner } from "react-barcode-scanner";
 import "react-barcode-scanner/polyfill";
+import { env } from "~/env";
+
+interface DimensionsStructured {
+  length: {
+    unit: string;
+    value: number;
+  };
+  width: {
+    unit: string;
+    value: number;
+  };
+  weight: {
+    unit: string;
+    value: number;
+  };
+  height: {
+    unit: string;
+    value: number;
+  };
+}
+
+interface Book {
+  publisher: string;
+  synopsis: string;
+  language: string;
+  image: string;
+  title_long: string;
+  dimensions: string; // This can be a more complex structure if needed
+  dimensions_structured: DimensionsStructured;
+  pages: number;
+  date_published: number;
+  subjects: string[];
+  authors: string[];
+  title: string;
+  isbn13: string;
+  msrp: string;
+  binding: string;
+  isbn: string;
+  isbn10: string;
+}
+
+interface Data {
+  book: Book;
+}
+
+interface BarcodeData {
+  rawValue: string;
+}
 
 export default function Home() {
   const [text, setText] = useState("");
   const [image, setImage] = useState("");
   const [open, setOpen] = useState(false);
 
-  let headers = {
+  const headers = {
     "Content-Type": "application/json",
-    Authorization: "54846_317405f0b131a44b60976bc4513a0284",
+    Authorization: env.ISBN_AUTH,
   };
 
   const getBook = async (book: string) => {
-    const res = await fetch(`https://api2.isbndb.com/book/${book}`, {
-      headers: headers,
-    });
-    const data = await res.json();
-    setText(await data.book.title);
-    setImage(await data.book.image);
-    setOpen(false);
-    console.log(await data);
+    try {
+      const res = await fetch(`https://api2.isbndb.com/book/${book}`, {
+        headers: headers,
+      });
+
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = (await res.json()) as Data;
+
+      setText(data.book.title); // No need for await here
+      setImage(data.book.image); // No need for await here
+      setOpen(false);
+      console.log(data); // No need for await here
+    } catch (error) {
+      console.error("Failed to fetch book data:", error);
+    }
   };
 
   return (
     <div className="App">
-      <button onClick={() => setOpen(!open)}>Scan Book </button>
+      <button onClick={() => setOpen(!open)}>Scan Book</button>
       {open && (
         <BarcodeScanner
-          onCapture={(i) => getBook(i.rawValue)}
+          onCapture={(i: BarcodeData) => getBook(i.rawValue)}
           options={{ formats: ["ean_13"] }}
         />
       )}
-      <div>Results : {text}</div>
-      {image && <img src={image} />}
+      <div>Results: {text}</div>
+      {image && <img src={image} alt="Book Cover" />}
     </div>
   );
 }
--- src/env.js
+++ src/env.js
@@ -11,6 +11,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    ISBN_AUTH: z.string(),
   },
 
   /**
@@ -29,6 +30,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    ISBN_AUTH: process.env.ISBN_AUTH,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
